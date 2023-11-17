c64feb5e08a9bb662ccc4e6778faee5057e8751f
LuisAxelMarínGarcía <213467@ids.upchiapas.edu.mx>
2023-11-16 23:16:25 -0600

    nuevo login

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,48 +1,44 @@
-    import React, { useState } from 'react';
-    import axios from 'axios';
-    import { useNavigate } from 'react-router-dom';
-    import './login.css';
+import React, { useState } from 'react';
+import './login.css';
 
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-    function Login() {
-    const [message, setMessage] = useState('');
-    const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Iniciando sesión con:', email, password);
+  };
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        const email = e.target.email.value;
-        const password = e.target.password.value;
+  return (
+    <div className="auth-page">
+      <div className="auth-container">
+        <h2>Iniciar Sesión</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="email">Correo electrónico</label>
+            <input
+              type="text"
+              id="email"
+              value={username}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Contraseña</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Iniciar Sesión</button>
+        </form>
+        <p>¿No tienes una cuenta? Regístrate</p>
+      </div>
+    </div>
+  );
+};
 
-        try {
-        const response = await axios.post('http://3.224.141.144/api/auth/token', { email, password });
-        console.log(response.data);
-        setMessage('Inicio de sesión exitoso!');
-        navigate('/');
-        } catch (error) {
-        console.error(error);
-        setMessage('Error en el inicio de sesión');
-        }
-    };
-
-    return (
-        <div className="auth-page">
-        <div className="auth-container">
-            <h2>Iniciar sesión</h2>
-            <form onSubmit={handleLogin}>
-            <div className="form-group">
-                <label htmlFor="email">Correo electrónico:</label>
-                <input type="email" id="email" name="email" required />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Contraseña:</label>
-                <input type="password" id="password" name="password" required />
-            </div>
-            <button type="submit">Iniciar sesión</button>
-            </form>
-            {message && <p>{message}</p>}
-        </div>
-        </div>
-    );
-    }
-
-    export default Login;
+export default Login;
